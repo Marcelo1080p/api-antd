434ef6d1250a4d6ac2db7c02d830dcf53fc80cf5
Marcelo1080p <juniooor.2015@gmail.com>
2023-01-03 21:05:42 -0300

    excluido arquivos desnecessarios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Avatar, Button, List, Skeleton } from "antd";
+import axios from "axios";
+const count = 3;
+const App = () => {
+  const [initLoading, setInitLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [list, setList] = useState([]);
+  
 
-function App() {
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character`)
+      .then((res) => {
+        setInitLoading(false);
+        setData(res.data.results);
+        setList(res.data.results);
+      });
+  }, []);
+
+
+
+  const onLoadMore = () => {
+    setLoading(true);
+    setList(
+      data.concat(
+        [...new Array(count)].map(() => ({
+          loading: true,
+          name: {},
+          picture: {},
+        }))
+      )
+    );
+
+  };
+
+  
+  const loadMore =
+    !initLoading && !loading ? (
+      <div
+      style={{
+        textAlign: "center",
+        marginTop: 12,
+        height: 32,
+        lineHeight: "32px",
+      }}
+      >
+        <Button onClick={onLoadMore}>loading more</Button>
+      </div>
+    ) : null;
+    
+    const cores = (status) => status === "Alive" ? "green" : status === "Dead" ? "red" : "grey";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="list">
+      <List
+        className="demo-loadmore-list"
+        loading={initLoading}
+        itemLayout="horizontal"
+        loadMore={loadMore}
+        dataSource={list}
+        renderItem={(item) => (
+          <List.Item
+            actions={[
+              <a key="list-loadmore-edit">edit</a>,
+              <a key="list-loadmore-more">more</a>,
+            ]}
+          >
+            <Skeleton avatar title={false} loading={item.loading} active
+            >
+              <List.Item.Meta
+                
+                avatar={<Avatar src={item.image} />}
+                title={<a href={item.url}>{item.name}</a>}
+                description={<p style={{color: cores(item.status), paddin: 0, margin: 0}}>{item.status}</p>}
+                
+              />
+              
+              <div>{item.species}</div>
+            </Skeleton>
+          </List.Item>
+        )}
+      />
     </div>
   );
-}
+};
 
 export default App;
